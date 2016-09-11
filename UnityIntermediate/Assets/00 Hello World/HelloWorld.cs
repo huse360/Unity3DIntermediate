e6a0536d9fd3e2dacf0b9ba0471aab17fa8d2b64
huso <huse360@gmail.com>
2016-09-11 16:41:31 +0900

    Add source files

---
+++ UnityIntermediate/Assets/00 Hello World/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Hola Mundo!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ UnityIntermediate/Assets/02 Hello Quad/HelloQuad.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloQuad : MonoBehaviour {
+
+	Mesh mesh;
+	MeshRenderer meshRenderer;
+	MeshFilter meshFilter;
+	Vector3[] vertices;
+	int[] triangles;
+	
+	public Material material;
+
+	// Use this for initialization
+	void Start () {
+	
+		meshFilter = gameObject.AddComponent<MeshFilter>();
+		meshRenderer = gameObject.AddComponent<MeshRenderer>();
+		
+		meshRenderer.material = material;
+		
+		mesh = new Mesh();
+		meshFilter.mesh = mesh;
+		
+		vertices = new[] {
+			new Vector3(0,0,0),
+			new Vector3(0,1,0), 
+			new Vector3(1,0,0),
+			new Vector3(1,1,0),
+
+		};
+		
+		mesh.vertices = vertices;
+		
+		triangles = new[]{
+			0, 1, 2,
+			2, 1, 3, 
+		};
+		
+		mesh.triangles = triangles;
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
