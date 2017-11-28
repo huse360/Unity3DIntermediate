4e364914406b663394f80e06a8102a93757f06dd
huse <huse360@users.noreply.github.com>
2017-11-28 19:26:35 +0900

    add comment

--- Geometry/Cylinder.cs
+++ Geometry/Cylinder.cs
@@ -1,3 +1,4 @@
+/*
 Copyright (c) <2017> <HUSSEIN NAZARALA>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -17,7 +18,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-
+*/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
