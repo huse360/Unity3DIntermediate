818c3532f6d88e278f9ed17de3f5611d0aa4482d
huse <huse360@users.noreply.github.com>
2019-08-07 18:42:53 -0500

    Rotar con fisica

--- Quaternions/MouseRotation.cs
+++ Quaternions/MouseRotation.cs
@@ -2,19 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseRotation : MonoBehaviour
+public class BoxRotation : MonoBehaviour
 {
    
 
     Vector2 mouseDelta = Vector2.zero;
 
     //la "velocidad" de giro
-    public float sensitivity = 0.05f;
+    public float sensitivity = 10.0f;
 
     Vector2 amount = Vector2.zero;
 
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 
         mouseDelta.Set(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
@@ -23,12 +30,17 @@ public class MouseRotation : MonoBehaviour
 
         //amplificar o reducir el movimiento segun la sensibilidad
         amount += mouseDelta * sensitivity;
-        amount.y = Mathf.Clamp(amount.y, -90, 90);  //Restringir esta rotacion (Comenta esta linea si quieres)
+        //amount.y = Mathf.Clamp(amount.y, -90, 90);  //Restringir esta rotacion (Comenta esta linea si quieres)
 
         //Mezclar las dos rotaciones:
-        transform.rotation = 
-            Quaternion.AngleAxis(amount.x, Vector3.up)
-            * Quaternion.AngleAxis(amount.y, Vector3.right);
+        //transform.rotation = 
+        //Quaternion.AngleAxis(amount.x, Vector3.up)
+        //* Quaternion.AngleAxis(amount.y, Vector3.right);
+
+        //Rotar la fisica:
+        rb.MoveRotation(Quaternion.AngleAxis(amount.x, Vector3.up)
+                        * Quaternion.AngleAxis(amount.y, Vector3.right));
+
 
     }
 }
