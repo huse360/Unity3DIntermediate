c7cd0b083c07e36aa8c7cc08742ca13e0630e7af
huse <huse360@users.noreply.github.com>
2019-08-14 00:38:10 -0500

    Add files via upload

---
+++ QuickTips/RotateAround.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+using static Unity.Mathematics.math;
+using Unity.Mathematics;
+
+public class RotateAround : MonoBehaviour
+{
+    public Transform target;
+    public float distance = 3;
+    public float speed = 30;
+    public float angSpeed = 20;
+
+    double sinA, cosA;
+    float3 pos;
+    float t = 0.0f;
+
+    void Update()
+    {
+        transform.rotation *= Quaternion.AngleAxis(angSpeed * Time.deltaTime, Vector3.up);
+
+        t += Time.deltaTime * speed * 0.1f;
+
+        sincos(t, out sinA, out cosA);
+
+        pos = float3(distance * (float)cosA, transform.position.y, distance * (float)sinA);
+
+        transform.position = pos;
+        transform.position += target.position;
+
+    }
+}
